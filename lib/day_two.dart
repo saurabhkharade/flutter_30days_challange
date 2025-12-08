37c035d98e84229fc242dd2322ab8a6799d38e64
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-12-08 23:20:18 +0530

    Day 2: Added Flutter Container, Text, Icon & Button UI

---
+++ lib/day_two.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DayTwoScreen extends StatelessWidget {
+  const DayTwoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text("Day 2 - Flutter Study"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(50),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 12,
+                color: Colors.black.withOpacity(0.1),
+                offset: const Offset(0, 6),
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(Icons.flutter_dash, size: 80),
+              const SizedBox(height: 20),
+              const Text(
+                "Flutter Day 2",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Today I learned basic UI structure, widgets, "
+                    "Container, Text, Button, Icon & simple styling.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("Continue Learning"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_30days_challange/day_two.dart';
 
-import 'homepage.dart';
+import 'day_one.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomeScreen(),
+      home: DayTwoScreen(),
     );
   }
 }
